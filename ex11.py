ab82ebff1719fa967f471729e480ee73090df1b7
drmetz2120 <d.metz2120@gmail.com>
2015-02-07 01:38:02 -0500

    Delete ex11.py

--- ex11.py
+++
@@ -1,8 +0,0 @@
-print "How tall are you?",
-height = raw_input()
-print "How old are you?",
-age = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
-
-print "So you're %s years old, %s tall, and weigh %s pounds." % (age, height, weight)
